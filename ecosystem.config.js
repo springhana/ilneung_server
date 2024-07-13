76514665e92fbe6608c0c606e4fb5895e48e88cc
springhana <xksxksanfro7@naver.com>
2024-07-13 23:05:31 +0900

    ⚙️setting:  script 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "ilneung_server",
-      script: "/dist/main",
+      script: "dist/main",
       instances: 1,
       autorestart: true,
       watch: false,
